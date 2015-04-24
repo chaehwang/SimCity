f49bccad0de5a696117887fe99d74744c84143c9
Kevin <khuang01@college.harvard.edu>
2015-04-23 20:39:02 -0400

    test

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "platform.h"
- 
+#include "platform.c"
+
 int main()
-{ 
-  //printf("%d\n", test_connected1());
-  test_degrees(); 
-  test_distance(); 
+{
+  printf("Should return true: %d\n", test_connected1());
+  printf("Should return false: %d\n", test_connected2());
+  test_degrees();  
   
-}               
+  /* int *test = malloc(12 * sizeof(road_construction)); */
+  
+  /* if (test[0] == NULL)
+    printf("hi"); */ 
+  /* test[5] = 10;
+  test[3] = 8;
+  for (int i = 0; i < 12; i++)
+  {
+    printf("%d\n", test[i]);
+  }
+  printf("%d\n", sizeof(int)); */
+}
